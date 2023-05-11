1c209d55771d50662471650a6c08823005f96bac
Salvador Madrid <124403557+GiacomoMadrid@users.noreply.github.com>
2023-05-11 17:36:22 -0500

    Update App.java

--- Semana6/App/src/app/App.java
+++ Semana6/App/src/app/App.java
@@ -1,20 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+import java.util.Scanner
 package app;
-
-/**
- *
- * @author AULAFISI
- */
 public class App {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner jin = new Scanner(System.in);
+        
+        Docente docente = new Docente ();
+        
+        
+        
+        
+        
+        
+        
+        
     }
     
 }
